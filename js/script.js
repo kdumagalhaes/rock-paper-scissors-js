4d5b8af7f61d18e9e9589bc047d1104c38b4f88c
kdumagalhaes <ce.magalhaespereira@gmail.com>
2020-07-06 19:58:20 -0300

    commenting the code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 
-const game = () => {
+const rpsGame = () => {
     const startBtn = document.getElementById('start-button')
     const progressBar = document.getElementById("bar");
     const startGame = document.getElementById('start-game')
     const gameStarted = document.getElementById('game-started')
     const playersName = document.getElementById('players-name')
     const labelName = document.getElementById('label-name')
+
+    const userChoseRock = document.getElementById('userChoseRock')
+    const userChosePaper = document.getElementById('userChosePaper')
+    const userChoseScissor = document.getElementById('userChoseScissor')
+
+    const compChoseRock = document.getElementById('compChoseRock')
+    const compChosePaper = document.getElementById('compChosePaper')
+    const compChoseScissor = document.getElementById('compChoseScissor')
+
+    //getting names
+      function gettingNames() {
+        playersName.addEventListener('change', (e) => {
+          let userName = document.querySelectorAll('.my-name')
+          let nameValue = e.target.value
+          userName.forEach((_, i) => {
+            userName[i].textContent = nameValue
+          })
+      })
+    }
 
     //starting and loading game when click enter
     window.addEventListener('keyup', (e) => {
@@ -44,50 +63,77 @@ const game = () => {
 
     //user selections
     function userSelections() {
-      const userChoseRock = document.getElementById('userChoseRock')
-      userChoseRock.addEventListener('click', changeColorSelected)
-      const userChosePaper = document.getElementById('userChosePaper')
-      userChosePaper.addEventListener('click', changeColorSelected)
-      const userChoseScissor = document.getElementById('userChoseScissor')
-      userChoseScissor.addEventListener('click', changeColorSelected)
+      userChoseRock.addEventListener('click', changeColorUserSelected)
+      userChoseRock.addEventListener('click', computerSelections)
+      userChoseRock.addEventListener('click', whoIsTheWinner)
+      userChosePaper.addEventListener('click', changeColorUserSelected)
+      userChosePaper.addEventListener('click', computerSelections)
+      userChoseScissor.addEventListener('click', changeColorUserSelected)
+      userChoseScissor.addEventListener('click', computerSelections)
+    }
 
-      function changeColorSelected() {
-          this.style.color = 'var(--orange)'
-          this.style.opacity = 1
+    //change user selection color
+    function changeColorUserSelected() {
+      this.style.color = 'var(--orange)'
+      this.style.opacity = 1
+    }
+
+    //computer selections
+    function computerSelections() {
+
+      let computerOptions = ['rock', 'paper', 'scissor']
+      let computerRandomChoice = Math.floor(Math.random() * 3)
+      const computerChoice = computerOptions[computerRandomChoice]
+
+      //paiting selections    
+      if(computerChoice === 'rock') {
+        changeColorComputerSelected(compChoseRock)
+      } else if (computerChoice === 'paper') {
+        changeColorComputerSelected(compChosePaper)
+      } else {
+        changeColorComputerSelected(compChoseScissor)
       }
     }
 
-    //getting names
-    function gettingNames() {
-      playersName.addEventListener('change', (e) => {
-        let userName = document.querySelectorAll('.my-name')
-        let nameValue = e.target.value
-        userName.forEach((_, i) => {
-          userName[i].textContent = nameValue
-        })
+    //change computer selection color
+    function changeColorComputerSelected(option) {
+      option.style.color = 'var(--orange)'
+      option.style.opacity = 1
+    }
 
-      })
+    //verifying who's the winner
+    function whoIsTheWinner(userHand) {
+      if(userHand.target.id === 'userChoseRock') {
+        console.log('empateee')
+      }
     }
     
     //audio effects
-    startBtn.addEventListener('click', startEff)
+    function audioEffects() {
+      startBtn.addEventListener('click', startEff)
     
-    const startSound = new Audio()
-    startSound.src = ('./resources/start.mp3')
-    
-    async function startEff() {
-        await startSound.play()
-    }
-    
-    const moveSound = new Audio()
-    moveSound.src = ('./resources/move.mp3')
-    
-    async function moveEff() {
-        await moveSound.play();
+      const startSound = new Audio()
+      startSound.src = ('./resources/start.mp3')
+      
+      async function startEff() {
+          await startSound.play()
+      }
+  
+      userChoseRock.addEventListener('click', moveEff)
+      userChosePaper.addEventListener('click', moveEff)
+      userChoseScissor.addEventListener('click', moveEff)
+      
+      const moveSound = new Audio()
+      moveSound.src = ('./resources/move.mp3')
+      
+      async function moveEff() {
+          await moveSound.play();
+      }
     }
 
+  audioEffects()
   gettingNames()
   userSelections()
 }
 
-game()
+rpsGame()
